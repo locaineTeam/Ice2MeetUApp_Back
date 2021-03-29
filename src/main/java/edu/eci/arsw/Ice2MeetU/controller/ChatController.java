a392a924e04fdb0872363d6f7f64187ee5a1683c
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-03-28 21:54:26 -0500

    Back Chat

--- src/main/java/edu/eci/arsw/Ice2MeetU/controller/ChatController.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/controller/ChatController.java
@@ -1,4 +1,31 @@
 package edu.eci.arsw.Ice2MeetU.controller;
 
+import edu.eci.arsw.Ice2MeetU.entities.Chat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+
+
+
+        @MessageMapping("/chat.sendMessage")
+        @SendTo("/topic/javainuse")
+        public Chat sendMessage(@Payload Chat chat) {
+            return chat;
+        }
+
+        @MessageMapping("/chat.newUser")
+        @SendTo("/topic/javainuse")
+        public Chat newUser(@Payload Chat chat,
+                                            SimpMessageHeaderAccessor headerAccessor) {
+            headerAccessor.getSessionAttributes().put("username", chat.getSender());
+            return chat;
+        }
+
+
 }
--- src/main/java/edu/eci/arsw/Ice2MeetU/entities/Chat.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/entities/Chat.java
@@ -1,4 +1,32 @@
 package edu.eci.arsw.Ice2MeetU.entities;
 
 public class Chat {
+        private String type;
+        private String content;
+        private String sender;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public void setSender(String sender) {
+            this.sender = sender;
+        }
+
 }
