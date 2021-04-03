f12c2a753701e9bedd2816a2c03afbf802058926
James Weisnner <james.weisnner@mail.escuelaing.edu.co>
2021-04-02 22:03:03 -0500

    revert deploy

--- src/main/java/edu/eci/arsw/Ice2MeetU/App.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/App.java
@@ -1,19 +1,22 @@
 package edu.eci.arsw.Ice2MeetU;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Hello world!
  *
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
     {
+        SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
