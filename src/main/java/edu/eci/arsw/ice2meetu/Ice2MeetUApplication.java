f7aaeb6d44dbc0cd9a96206d76d6e1e01b848206
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-04-12 00:36:36 -0500

    All Back-chat

---
+++ src/main/java/edu/eci/arsw/ice2meetu/Ice2MeetUApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ice2meetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ice2MeetUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ice2MeetUApplication.class, args);
+	}
+
+}
