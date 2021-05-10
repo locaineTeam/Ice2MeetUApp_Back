bb1c2e0799d8d0f5a359d6ec08ca7a859e1f7f44
Simón Marín <german.marin@mail.escuelaing.edu.co>
2021-05-09 22:22:32 -0500

    Charles?

--- src/main/java/edu/eci/arsw/Ice2MeetU/persistence/cache/impl/JedisConfig.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/persistence/cache/impl/JedisConfig.java
@@ -2,6 +2,7 @@ package edu.eci.arsw.Ice2MeetU.persistence.cache.impl;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,13 +12,16 @@ public class JedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory(){
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration("70.37.64.238", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
 
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
         RedisTemplate<String,Object> template = new RedisTemplate<>();
+
         JedisConnectionFactory f = jedisConnectionFactory();
         template.setConnectionFactory(f);
 
--- src/main/java/edu/eci/arsw/Ice2MeetU/twilioToken/TwilioToken.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/twilioToken/TwilioToken.java
@@ -27,7 +27,7 @@ public class TwilioToken {
 
         }
         else{
-
+            System.out.println("jojojojo");
             String twilioAccountSid = "AC4ac6afd395a417a6a61179945e8abef0";
             String twilioApiKey = "SKc346bdc0cabf5c15a9cb850024acd47a";
             String twilioApiSecret = "V7gthu7BMNkuxVqQyQrzJLf0r9rO8fnn";
@@ -52,6 +52,10 @@ public class TwilioToken {
             AccessToken token = new AccessToken.Builder(twilioAccountSid, twilioApiKey, twilioApiSecret)
                     .identity(identity).grant(grant).grant(grantVoice).grant(grantVideo).build();
 
+            for (int i = 0; i < 10000000; i++) {
+                i += 0;
+
+            }
             //System.out.println(token.toJwt());
             t = new Token(token.toJwt());
             tokenCacheRedis.set(user,t,5*60);
