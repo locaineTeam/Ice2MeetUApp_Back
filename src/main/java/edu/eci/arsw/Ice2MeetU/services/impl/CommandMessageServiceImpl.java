4b498c31413d29629d01c209ef4f3a2d33485dfb
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-05-18 23:31:48 -0500

    corrección cositas

--- src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
@@ -22,20 +22,26 @@ public class CommandMessageServiceImpl implements CommandMessageService {
     public CommandMessageServiceImpl(){
         // <editor-fold defaultstate="collapsed" desc="Spanish Commands">
         Commands command = new Commands();
-        command.addCommand("ayuda","#AYUDA - shows all commands  </br> #HOLA - shows a hello!");
+        command.addCommand("ayuda","#AYUDA - muestra todos los comandos </br> #HOLA - Muestra un hola! </br> #COMTAS - Saluda con un como estás </br> #CHAU - Despide");
         command.addCommand("hola","HOLA!");
+        command.addCommand("comtas","¿Cómo estás?");
+        command.addCommand("chau","Adios! Hasta la vista!");
         commands.put("spanish",command);
         // </editor-fold">
         // <editor-fold defaultstate="collapsed" desc="English Commands">
         command = new Commands();
-        command.addCommand("help","#HELP - shows all commands  </br>  #HELLO - shows a hello");
-        command.addCommand("hello","HELLO!");
+        command.addCommand("help","#HELP - shows all commands  </br>  #HELLO - shows a hello </br> #HOWRU - shows a How are you </br> #BYE - shows a bye");
+        command.addCommand("hello","Hello!");
+        command.addCommand("howru","Hey! How are you?");
+        command.addCommand("bye","bye!");
         commands.put("english",command);
         // </editor-fold">
         // <editor-fold defaultstate="collapsed" desc="German Commands">
         command = new Commands();
-        command.addCommand("Hilfe","#HILFE - shows all commands </br> #HALLO - shows a hello!");
-        command.addCommand("hallo","HALLO!");
+        command.addCommand("hilfe","#HILFE - zeigt alle Befehle </br> #HALLO - zeigt hallo! </br> #WGEHT - zeigt wie geht es dir </br> #TSCH - zeigt wie geht es dir");
+        command.addCommand("hallo","Hallo!");
+        command.addCommand("wgeht","wie gehts?");
+        command.addCommand("tsch","Tschüss");
         commands.put("german",command);
         // </editor-fold">
     }
@@ -44,7 +50,6 @@ public class CommandMessageServiceImpl implements CommandMessageService {
     public String getMessageByCommandAndLanguage(String command, String language) throws CommandMessagesException {
         command = command.toLowerCase();
         language = language.toLowerCase();
-        System.out.println("ALL VALUE: "+ commands.toString());
         if(!commands.containsKey(language)) throw new CommandMessagesException(CommandMessagesException.LANGUAGE_NO_FOUND,language);
         String message = commands.get(language).getMessageByCommand(command);
         if(message.equals("")) throw new CommandMessagesException(CommandMessagesException.COMMAND_NO_FOUND,command);
