2ec322b4271702745ab33557db3207429b32e60c
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-05-19 03:21:44 -0500

    Se agregan más icebreakers

--- src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
@@ -22,7 +22,7 @@ public class CommandMessageServiceImpl implements CommandMessageService {
     public CommandMessageServiceImpl(){
         // <editor-fold defaultstate="collapsed" desc="Spanish Commands">
         Commands command = new Commands();
-        command.addCommand("ayuda","#AYUDA - muestra todos los comandos </br> #HOLA - Muestra un hola! </br> #COMTAS - Saluda con un como estás </br> #CHAU - Despide");
+        command.addCommand("ayuda","#AYUDA - muestra todos los comandos \n #HOLA - Muestra un hola! \n #COMTAS - Saluda con un como estás \n #CHAU - Despide");
         command.addCommand("hola","HOLA!");
         command.addCommand("comtas","¿Cómo estás?");
         command.addCommand("chau","Adios! Hasta la vista!");
@@ -30,7 +30,7 @@ public class CommandMessageServiceImpl implements CommandMessageService {
         // </editor-fold">
         // <editor-fold defaultstate="collapsed" desc="English Commands">
         command = new Commands();
-        command.addCommand("help","#HELP - shows all commands  </br>  #HELLO - shows a hello </br> #HOWRU - shows a How are you </br> #BYE - shows a bye");
+        command.addCommand("help","#HELP - shows all commands  \\n  #HELLO - shows a hello \\n #HOWRU - shows a How are you \\n #BYE - shows a bye");
         command.addCommand("hello","Hello!");
         command.addCommand("howru","Hey! How are you?");
         command.addCommand("bye","bye!");
@@ -38,7 +38,7 @@ public class CommandMessageServiceImpl implements CommandMessageService {
         // </editor-fold">
         // <editor-fold defaultstate="collapsed" desc="German Commands">
         command = new Commands();
-        command.addCommand("hilfe","#HILFE - zeigt alle Befehle </br> #HALLO - zeigt hallo! </br> #WGEHT - zeigt wie geht es dir </br> #TSCH - zeigt wie geht es dir");
+        command.addCommand("hilfe","#HILFE - zeigt alle Befehle \n #HALLO - zeigt hallo! \n #WGEHT - zeigt wie geht es dir \n #TSCH - zeigt wie geht es dir");
         command.addCommand("hallo","Hallo!");
         command.addCommand("wgeht","wie gehts?");
         command.addCommand("tsch","Tschüss");
--- src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/IceBreakerServiceImpl.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/IceBreakerServiceImpl.java
@@ -21,21 +21,30 @@ public class IceBreakerServiceImpl implements IceBreakerService {
     public static final ArrayList<String> SPANISH_ICE_BREAKER = new ArrayList<String>(
             Arrays.asList("¿Cuantos colores tiene el arcoiris?",
                     "¿Qué fue primero, el huevo o la gallina?",
-                    "Crees en los fantasmas?"));
+                    "Crees en los fantasmas?",
+                    "Si pudieras tener algún súper poder, ¿cuál sería?",
+                    "Levanten la mano que prefieren ¿los gatos o los perros?",
+                    "¿Puedes contarme sobre el día más emocionante de tu vida?"));
     /**
      * The constant ENGLISH_ICE_BREAKER.
      */
     public static final ArrayList<String> ENGLISH_ICE_BREAKER = new ArrayList<String>(
             Arrays.asList("How many colors does the rainbow have?",
                     "What was the first; the egg or the chicken?",
-                    "Do you believe in ghosts?"));
+                    "Do you believe in ghosts?",
+                    "If you could have any super powers, what would it be?",
+                    "Raise the hand that cats or dogs prefer?",
+                    "Can you tell me about the most exciting day of your life?"));
     /**
      * The constant GERMAN_ICE_BREAKER.
      */
     public static final ArrayList<String> GERMAN_ICE_BREAKER = new ArrayList<String>(
             Arrays.asList("Wie viele Farben hat der Regenbogen?",
                     "Was war das erste, das Ei oder das Huhn?",
-                    "Glaubst du an Geister?"));
+                    "Glaubst du an Geister?",
+                    "Wenn du irgendwelche Superkräfte haben könntest, welche wären das?",
+                    "Die Hand heben, die Katzen oder Hunde bevorzugen?",
+                    "Kannst du mir etwas über den aufregendsten Tag deines Lebens erzählen?"));
 
     public IceBreaker getRandomIceBreaker(String language){
         ArrayList <String> chosenLanguage;
