efcaaa8551a15d6f7278713564cce7d58e2da078
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-05-24 23:02:45 -0500

    Ajuste para aceptar cualquier versión del comando

--- src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
+++ src/main/java/edu/eci/arsw/Ice2MeetU/services/impl/CommandMessageServiceImpl.java
@@ -6,6 +6,8 @@ import edu.eci.arsw.Ice2MeetU.persistence.exceptions.CommandMessagesException;
 import edu.eci.arsw.Ice2MeetU.services.CommandMessageService;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -15,6 +17,7 @@ import java.util.HashMap;
 public class CommandMessageServiceImpl implements CommandMessageService {
 
     private HashMap<String, Commands> commands = new HashMap<>();
+    private HashMap<String, String[]> pairs = new HashMap<>();
 
     /**
      * Instantiates a new Command message service.
@@ -44,6 +47,50 @@ public class CommandMessageServiceImpl implements CommandMessageService {
         command.addCommand("tsch","Tschüss");
         commands.put("german",command);
         // </editor-fold">
+
+        addToPairs();
+    }
+
+    private void addToPairs() {
+        // <editor-fold defaultstate="collapsed" desc="Hello Group">
+        String[] aux={ "hola", "hallo"};
+        pairs.put("hello",aux);
+        aux= new String[]{ "hello", "hallo"};
+        pairs.put("hola",aux);
+        aux= new String[]{ "hola", "hello"};
+        pairs.put("hallo",aux);
+        // </editor-fold">
+        // <editor-fold defaultstate="collapsed" desc="Help Group">
+        aux= new String[]{ "help", "ayuda"};
+        pairs.put("hilfe",aux);
+        aux= new String[]{ "ayuda", "hilfe"};
+        pairs.put("help",aux);
+        aux= new String[]{ "help", "hilfe"};
+        pairs.put("ayuda",aux);
+        // </editor-fold">
+        // <editor-fold defaultstate="collapsed" desc="HRU Group">
+        aux= new String[]{ "comtas", "howru"};
+        pairs.put("wgeht",aux);
+        aux= new String[]{ "wgeht", "howru"};
+        pairs.put("comtas",aux);
+        aux= new String[]{ "comtas", "wgeht"};
+        pairs.put("howru",aux);
+        // </editor-fold">
+        // <editor-fold defaultstate="collapsed" desc="Bye Group">
+        aux= new String[]{ "chau", "bye"};
+        pairs.put("tsch",aux);
+        aux= new String[]{ "tsch", "bye"};
+        pairs.put("chau",aux);
+        aux= new String[]{ "tsch", "chau"};
+        pairs.put("bye",aux);
+        // </editor-fold">
+
+    }
+
+    private String[] getPairs(String key) throws CommandMessagesException {
+        if(!pairs.containsKey(key)) throw new CommandMessagesException(CommandMessagesException.LANGUAGE_NO_FOUND,key);
+        return pairs.get(key);
+
     }
 
     @Override
@@ -51,8 +98,16 @@ public class CommandMessageServiceImpl implements CommandMessageService {
         command = command.toLowerCase();
         language = language.toLowerCase();
         if(!commands.containsKey(language)) throw new CommandMessagesException(CommandMessagesException.LANGUAGE_NO_FOUND,language);
+        String[] pairs = getPairs(command);
         String message = commands.get(language).getMessageByCommand(command);
+        for (String s: pairs){
+            if(!commands.get(language).getMessageByCommand(s).equals("") && message.equals("")) {
+                message = commands.get(language).getMessageByCommand(s);
+            }
+        }
         if(message.equals("")) throw new CommandMessagesException(CommandMessagesException.COMMAND_NO_FOUND,command);
         return new CommandValue(message);
     }
+
+
 }
